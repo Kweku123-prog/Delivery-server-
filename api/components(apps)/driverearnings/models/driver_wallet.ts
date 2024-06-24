5eaa311365162fadee4ab5e1fdb86d8b1e1c96ea
kcqua <kweku.quansah@ashesi.edu.gh>
2024-06-24 00:44:02 +0000

    me

--- api/components(apps)/driverearnings/models/driver_wallet.ts
+++ api/components(apps)/driverearnings/models/driver_wallet.ts
@@ -1,7 +1,17 @@
 import mongoose from 'mongoose';
+import { stringsUtils } from '../../../utils';
 
 const EarningsSchema = new mongoose.Schema({
-  driverId: { type: String, required: true },
+  driverId: { 
+
+	type: String,
+			required: true,
+			default: () => stringsUtils.generateUniqueString(4),
+
+
+
+   },
+  
   earnings: { type: Number, required: true },
   date: { type: String, required: true },
 });
